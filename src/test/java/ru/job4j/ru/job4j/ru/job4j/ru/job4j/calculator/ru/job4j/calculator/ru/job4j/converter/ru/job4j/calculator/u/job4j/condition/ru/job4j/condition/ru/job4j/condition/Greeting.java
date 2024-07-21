b3b8d25e12c713424dc94ace4c17b46e0b1a3802
Stas Petrov <Milkispetrov@yandex.ru>
2024-07-21 19:24:16 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/test/java/ru/job4j/ru/job4j/ru/job4j/ru/job4j/calculator/ru/job4j/calculator/ru/job4j/converter/ru/job4j/calculator/u/job4j/condition/ru/job4j/condition/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.ru.job4j.ru.job4j.ru.job4j.calculator.ru.job4j.calculator.ru.job4j.converter.ru.job4j.calculator.u.job4j.condition.ru.job4j.condition.ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But i am a newbie ";
+        int year = 2024;
+        idea += year;
+        System.out.println(idea);
+    }
+}
